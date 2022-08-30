456f30f66730439e2909834701d87e2301d3c97e
Yusadolat <yusadolat@gmail.com>
2022-08-30 19:51:59 +0100

    chnage root text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Quran BOT Migrated to Keyob!'));
 
 
 var Twit = require('twit')
