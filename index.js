1af4a07f586b76a8c8743461e3cb80deeef53fe5
Yusadolat <yusadolat@gmail.com>
2022-08-30 19:51:00 +0100

    Default PORT to 3000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000;
 const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
